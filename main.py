c958721b89cf3ece6a3d4c82addbd163a882d33a
Janik von Ahnen <j.v.ahnen@gmx.de>
2019-10-12 01:19:54 +0200

    first session

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
-
-from kivy.app import App
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.app import App
+import kivy
+# import loginScreen.LoginScreen
 
+kivy.require("1.11.1")
+
+
+class MainScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        self.cols = 1
+        self.goLoginScreen=Button(text="button1")
+        self.goLoginScreen.bind(on_press=self.pressLoginScreen)
+        self.add_widget(self.goLoginScreen)
+
+    def pressLoginScreen(self, instance):
+        print("hello")
 
 class MyApp(App):
-
     def build(self):
-        return Label(text='Hello world')
+        return MainScreen()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
